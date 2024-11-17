e53c371b269d90c93425eec6bb6d57fd6ad11afe
Márton <szmarton08@gmail.com>
2024-11-17 16:41:10 +0100

    Created the Dynamic array. Made the Add, Insert, Delete, ListAll, thisEmpty methods

---
+++ ToDoConsoleApp/DynamicArray.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoConsoleApp
+{
+    internal class DynamicArray
+    {
+        public int size = 0;
+        public int capacity = 10;
+        string[] array = new string[10];
+
+
+
+        public void Add(string data)
+        {
+            if (size >= capacity)
+            {
+                grow();
+            }
+            
+            array[size] = data;
+            size++;
+        }
+
+        public void Insert(int index, string data)
+        {
+            if (size >= capacity)
+            {
+                grow();
+            }
+            else if (array[index] != null)
+            {
+                for (int i = size; i > index; i--)
+                {
+                    array[i] = array[i-1];
+                    array[i-1] = null;
+                }
+            }
+            array[index] = data;
+            size++;
+        }
+
+        public void delete(string data)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] == data)
+                {
+                    
+                    for (int j = i; j < size; j++)
+                    {
+                        array[j] = array[j+1];
+                    }
+                }
+                
+            }
+
+            size--;
+
+        }
+
+        public int search(string data) {
+
+            return -1;
+        }
+
+        private void grow()
+        {
+
+        }
+
+        private void shrink() { 
+        
+        }
+
+        public bool thisEmpty()
+        {
+            return size == 0;
+        }
+
+        public string ListAll()
+        {
+            string String = string.Empty;
+
+            for(int i = 0; i < size; i++)
+            {
+                String += array[i] + ", ";
+            }
+            if (String != string.Empty)
+            {
+               String = '[' + String.Substring(0, String.Length - 2) + ']'; //Removing the comma, from the end, adding '[' and ']'
+            }
+            else
+            {
+               String = "[]";
+            }
+            return String;
+        }
+
+    }
+}
---
+++ ToDoConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+namespace ToDoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray dynamicArray = new DynamicArray();
+            dynamicArray.Add("Alma");
+            dynamicArray.Add("Körte");
+            dynamicArray.Add("Néma");
+            dynamicArray.Add("Béla");
+
+            dynamicArray.Insert(2, "Léta");
+            dynamicArray.delete("Alma");
+
+
+
+            Console.WriteLine($"Elements: {dynamicArray.ListAll()}");
+            Console.WriteLine($"Current size: {dynamicArray.size}");
+            Console.WriteLine($"Current capacity: {dynamicArray.capacity}");
+        }
+    }
+}
