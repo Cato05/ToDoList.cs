98b6fe77df556a4b371d46144a51b46670bec6f9
Márton <szmarton08@gmail.com>
2024-11-24 18:21:47 +0100

    Main menu completed for now Next task is adding the *create new task*, and the *list all tasks* pages in different files.

--- ToDoConsoleApp/Program.cs
+++ ToDoConsoleApp/Program.cs
@@ -7,24 +7,26 @@ namespace ToDoConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to your To-Do list!\n Navigate with arrows, press enter to mark a task completed!");
+            DynamicArray ToDoList = new DynamicArray();
 
-            ConsoleKeyInfo key;
+            Console.WriteLine("Welcome to your To-Do list!\n Navigate with arrows, press enter to select!");
+
+            ConsoleKeyInfo key; 
             int option = 1;
-            bool done = false;
-            string marking = "\u001b[31m";
+            bool done = false; //Checks if user is done with checking the list
+            string marking = "\u001b[31m"; //Marking color for the selection
             int max = 2; //The amount of tasks on the list
             int min = 1; //If there are tasks, 1, otherwise 0
             (int left, int top) = Console.GetCursorPosition();
+
             while (!done)
             {
                 Console.SetCursorPosition(left, top);
 
-                Console.WriteLine($" {(option == 1 ? marking : "")}Task 1\u001b[0m");
-                Console.WriteLine($" {(option == 2 ? marking : "")}Task 2\u001b[0m");
+                Console.WriteLine($" {(option == 1 ? marking : "")}Add a task\u001b[0m");
+                Console.WriteLine($" {(option == 2 ? marking : "")}See tasks\u001b[0m");
 
                 key = Console.ReadKey(true);
-
 
                 switch (key.Key)
                 {
@@ -37,7 +39,10 @@ namespace ToDoConsoleApp
                         break;
 
                     case ConsoleKey.Enter:
-                        //delete element
+                        if (option == 1)
+                            continue; //Go to the "task creator", pass for now
+                        else if (option == 2)
+                            continue; //Go to list all the tasks, WIP as well
                         break;
 
                     case ConsoleKey.Escape:
@@ -49,6 +54,7 @@ namespace ToDoConsoleApp
                     option = min;
                 else if (option > max)
                     option = max;
+
             }
         }
     }
