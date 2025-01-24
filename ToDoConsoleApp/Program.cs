8f905cdd28afed0199a7312cd8e9dcd9fd4b3a3e
Szabó Márton 860 <szamar860@hengersor.hu>
2025-01-24 08:42:29 +0100

    Remember, set the cursor position before you write out the SelectionMenu

--- ToDoConsoleApp/Program.cs
+++ ToDoConsoleApp/Program.cs
@@ -11,23 +11,25 @@ namespace ToDoConsoleApp
         {
 
             DynamicArray ToDoList = new DynamicArray();
-            Console.WriteLine("Welcome to your To-Do list!\n Navigate with arrows, press enter to select!");
+            Console.WriteLine("Welcome to your To-Do list!\n Navigate with arrows, press enter to select! Press escape to exit.");
             string ToDoFile = "../../../ToDo/To-DoList.txt";
             StreamWriter sw = new StreamWriter(ToDoFile);
             ConsoleKeyInfo key; 
             int option = 1;
             bool done = false; //Checks if user is done with checking the list
             string marking = "\u001b[31m"; //Marking color for the selection
+            string markingColorTasks = "\u001B[32m";
             int max = 2; //The amount of tasks on the list
             int min = 1; //If there are tasks, 1, otherwise 0
             (int left, int bot) = Console.GetCursorPosition();
+            (int top, int right) = Console.GetCursorPosition();
 
             while (!done)
             {
                
 
                 Thread.Sleep(100);
-                Console.SetCursorPosition(left, bot);
+                Console.SetCursorPosition(right, bot);
                 Console.WriteLine($" {(option == 1 ? marking : "")}Add a task\u001b[0m");
                 Console.WriteLine($" {(option == 2 ? marking : "")}See tasks\u001b[0m");
 
@@ -71,10 +73,13 @@ namespace ToDoConsoleApp
                         {
                             Console.Clear();
                             Console.WriteLine($"You're tasks are below: ");
+                            string[] tasks = ToDoList.ListAll().Split(" ");
+                            for (int i = 0; i < tasks.Length; i++)
+                            {
+                                Console.WriteLine(tasks[i]);
+                            }
+                            (left, bot) = Console.GetCursorPosition();
                             
-                            Console.WriteLine(ToDoList.ListAll());
-                            
-                            (left , bot) = Console.GetCursorPosition();
                         }
                             
                         break;
@@ -89,7 +94,22 @@ namespace ToDoConsoleApp
                 else if (option > max)
                     option = max;
 
+                
+
             }
         }
     }
 }
+
+
+//Start the Completed tasks window!
+
+/*
+ Make the tasks readable with  for loop, and cw, as you have done in lines 31, ans 32.
+ Selection color will be green.
+ Upon pressing enter, the task will be moved to "CompletedTasks" array.
+ CompletedTasksWindow will be cleared every day, and every day, it writes out how many tasks have we completed the prevoius day.
+ Tasks array stays the same.
+ Add timeStamps to tasks.
+ Sort it by what has the user the shortes time for.
+ */
